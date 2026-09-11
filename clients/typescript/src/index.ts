49c92ed5029b9e0376edf82a20e2415df2c55088
Alexander Mills <alex@oresoftware.com>
2026-09-11 13:59:12 -0500

    feat: export TypeScript APM snapshot types

--- clients/typescript/src/index.ts
+++ clients/typescript/src/index.ts
@@ -2,6 +2,26 @@ export { Client } from "./client";
 export { configFromEnv } from "./config";
 export type { ClientConfig } from "./config";
 export { ClientError } from "./errors";
-export type { Health, ResourceEnvelope } from "./types";
+export type {
+  ApmCapabilitySet,
+  ApmCapabilityStatus,
+  ApmSnapshot,
+  Attribute,
+  CorrelationContext,
+  Exemplar,
+  FilesystemSnapshot,
+  Health,
+  HistogramBucket,
+  HistogramPoint,
+  MetricKind,
+  MetricPoint,
+  ProcessSnapshot,
+  ProfileCapability,
+  ProfileKind,
+  ProfileSummary,
+  ResourceEnvelope,
+  RuntimeMetric,
+  RuntimeSnapshot,
+  ServicePerformanceSnapshot,
+} from "./types";
 export { RESOURCE } from "./types";
-
