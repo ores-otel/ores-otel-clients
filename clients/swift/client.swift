b8a5835a8f5c6f51a09ec7c6b0afd9252aa41d2c
Alexander Mills <alex@oresoftware.com>
2026-09-21 08:46:31 -0500

    DEN-3194 add health surface to Swift client

--- clients/swift/client.swift
+++ clients/swift/client.swift
@@ -1,1 +1,7 @@
-public struct Client { public let baseUrl: String }
+public struct Client {
+    public let baseUrl: String
+
+    public func healthUrl() -> String {
+        return baseUrl.trimmingCharacters(in: CharacterSet(charactersIn: "/")) + "/v1/health"
+    }
+}
