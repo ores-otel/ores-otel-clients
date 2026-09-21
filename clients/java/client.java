d6bede3b17aebf27798fe41d63ed3b8bfe86bb08
Alexander Mills <alex@oresoftware.com>
2026-09-21 08:45:59 -0500

    DEN-3194 add health surface to Java client

--- clients/java/client.java
+++ clients/java/client.java
@@ -1,2 +1,13 @@
 package dev.client;
-public final class Client { private Client() {} }
+
+public final class Client {
+    private final String baseUrl;
+
+    public Client(String baseUrl) {
+        this.baseUrl = baseUrl;
+    }
+
+    public String healthUrl() {
+        return baseUrl.replaceAll("/+$", "") + "/v1/health";
+    }
+}
